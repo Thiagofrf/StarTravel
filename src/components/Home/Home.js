8c8643211990d9233cf7d3bf83cc0a04a0429e68
Thiago Fontanella <thiago.fontanella@hotmail.com>
2021-05-26 11:00:23 -0300

    adicionando e consumindo json

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,43 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
 import TravelCard from '../TravelCard/TravelCard';
 import JakkuImg from '../../img/JakkuImg.png';
+import MustafarImg from '../../img/mustafar.jpg';
+import NabooImg from '../../img/naboo.jpg';
+import CoruscantImg from '../../img/coruscant.jpg';
+import { render } from '@testing-library/react';
 
 const Home = () => {
+  const [destinations, setDestinations] = React.useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/destinos')
+      .then((res) => res.json())
+      .then((result) => {
+        setDestinations(result);
+      });
+  }, []);
+
+  console.log('destinos', destinations);
   return (
     <div className="home-container">
       <h1>
         <span>Viaje na velocidade da luz</span> para destinos incríveis
       </h1>
       <div className="travel-card-carousel">
-        <TravelCard
-          category="Destinos"
-          title="Viaje para Jakku"
-          description="Absolutamente nada para fazer aqui!"
-          flag="Inclui um acompanhante"
-          image={JakkuImg}
-        />
-        <TravelCard
-          category="Destinos"
-          title="Viaje para Jakku"
-          description="Absolutamente nada para fazer aqui!"
-          flag="Inclui um acompanhante"
-          image={JakkuImg}
-        />
-        <TravelCard
-          category="Destinos"
-          title="Viaje para Jakku"
-          description="Absolutamente nada para fazer aqui!"
-          flag="Inclui um acompanhante"
-          image={JakkuImg}
-        />
-        <TravelCard
-          category="Destinos"
-          title="Viaje para Jakku"
-          description="Absolutamente nada para fazer aqui!"
-          flag="Inclui um acompanhante"
-          image={JakkuImg}
-        />
+        {destinations.map((item) => {
+          render(
+            <TravelCard
+              category={item.category}
+              title={item.title}
+              description={item.description}
+              flag={item.flag}
+              image={item.image}
+            />,
+          );
+        })}
       </div>
     </div>
   );
